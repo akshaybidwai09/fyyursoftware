bea4b02940ff3c57663614ededcedeb2e8ff070f
akshaybidwai09 <50581281+akshaybidwai09@users.noreply.github.com>
2021-07-18 10:40:43 +0530

    Add files via upload

---
+++ starter_code/model.py
@@ -0,0 +1,63 @@
+
+from flask_sqlalchemy import SQLAlchemy
+db = SQLAlchemy()
+
+
+
+# models
+class Venue(db.Model):
+    __tablename__ = 'Venue'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    address = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+
+
+    shows = db.relationship('Show', backref='Venue', lazy='joined',cascade='all, delete')
+
+    def __repr__(self):
+        return f'<Venue {self.id} {self.name}>'
+
+# Artist is the parent (one-to-many) of a Show (Venue is also a foreign key, in def. of Show)
+class Artist(db.Model):
+    __tablename__ = 'Artist'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    city = db.Column(db.String(120))
+    state = db.Column(db.String(120))
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)))
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean, default=False)
+    seeking_description = db.Column(db.String(120))
+
+    shows = db.relationship('Show', backref='Artist', lazy='joined',cascade='all, delete')   
+
+    def __repr__(self):
+        return f'<Artist {self.id} {self.name}>'
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = 'Show'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_time = db.Column(db.DateTime, nullable=False)   
+
+    # Foreign key is the tablename.pk
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)   
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Show {self.id} {self.start_time} artist_id={self.artist_id} venue_id={self.venue_id}>'
