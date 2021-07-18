56a892288a6513947d98d6f96a8f231da63e5209
akshaybidwai09 <50581281+akshaybidwai09@users.noreply.github.com>
2021-07-18 10:40:03 +0530

    modify

--- starter_code/app.py
+++ starter_code/app.py
@@ -8,12 +8,13 @@ import babel
 from flask_migrate import Migrate
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
 import logging
+from flask_sqlalchemy import SQLAlchemy
 from logging import Formatter, FileHandler
 from flask_wtf import FlaskForm as Form
 from forms import VenueForm, ArtistForm, ShowForm
 from forms import *
+from model import db, Venue, Show, Artist
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -22,71 +23,13 @@ from forms import *
 # TODO: connect to a local postgresql database
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app)
+db.init_app(app)
 moment = Moment(app)
 migrate = Migrate(app,db)
-#----------------------------------------------------------------------------#
-# Models.
-#----------------------------------------------------------------------------#
-
-# Venue is the parent (one-to-many) of a Show (Artist is also a foreign key, in def. of Show)
-
-class Venue(db.Model):
-    __tablename__ = 'Venue'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String(120)))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-    seeking_talent = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String(120))
 
 
-    shows = db.relationship('Show', backref='Venue', lazy=True)
 
-    def __repr__(self):
-        return f'<Venue {self.id} {self.name}>'
 
-# Artist is the parent (one-to-many) of a Show (Venue is also a foreign key, in def. of Show)
-class Artist(db.Model):
-    __tablename__ = 'Artist'
-
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
-    genres = db.Column(db.ARRAY(db.String(120)))
-    image_link = db.Column(db.String(500))
-    facebook_link = db.Column(db.String(120))
-    website_link = db.Column(db.String(120))
-    seeking_venue = db.Column(db.Boolean, default=False)
-    seeking_description = db.Column(db.String(120))
-
-    shows = db.relationship('Show', backref='Artist', lazy=True)   
-
-    def __repr__(self):
-        return f'<Artist {self.id} {self.name}>'
-
-# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
-class Show(db.Model):
-    __tablename__ = 'Show'
-
-    id = db.Column(db.Integer, primary_key=True)
-    start_time = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)   
-
-    # Foreign key is the tablename.pk
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable=False)   
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable=False)
-
-    def __repr__(self):
-        return f'<Show {self.id} {self.start_time} artist_id={self.artist_id} venue_id={self.venue_id}>'
 
 #----------------------------------------------------------------------------#
 # Filters.
@@ -374,8 +317,12 @@ def shows():
   # TODO: replace with real venues data.
   #       num_shows should be aggregated based on number of upcoming shows per venue.
   shows = Show.query.all()
-  data = []
   for show in shows:
+    show_query= db.session.query(Show, Artist, Venue).filter_by(Show.id == show.id).join(Artist,Artist.id== Show.artist_id).join(Venue,Venue.id == Show.venue_id)
+
+  results = show_query.all()
+  data = []
+  for show in results:
     data.append({
       "venue_id": show.venue_id,
       "venue_name": show.Venue.name,
